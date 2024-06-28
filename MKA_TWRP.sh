a6d74ecc51c8767aa5e1e1efa86b9ea7741f19f3
Massatrio16 <146039181+Massatrio16@users.noreply.github.com>
2024-06-28 14:54:01 +0700

    Update MKA_TWRP.sh

--- MKA_TWRP.sh
+++ MKA_TWRP.sh
@@ -28,13 +28,11 @@ read Build_Target
   apt update
   apt -y upgrade
   apt -y install gperf gcc-multilib gcc-10-multilib g++-multilib g++-10-multilib libc6-dev lib32ncurses5-dev x11proto-core-dev libx11-dev tree lib32z-dev libgl1-mesa-dev libxml2-utils xsltproc bc ccache lib32readline-dev lib32z1-dev liblz4-tool libncurses5-dev libsdl1.2-dev libwxgtk3.0-gtk3-dev libxml2 lzop pngcrush schedtool squashfs-tools imagemagick libbz2-dev lzma ncftp qemu-user-static libstdc++-10-dev libtinfo5
-   add-apt-repository universe
+   #add-apt-repository universe
+   apt install nano bc bison ca-certificates curl flex gcc git libc6-dev libssl-dev openssl python-is-python3 ssh wget zip zstd  make clang gcc-arm-linux-gnueabi software-properties-common build-essential libarchive-tools gcc-aarch64-linux-gnu -y &&  apt install build-essential -y &&  apt install libssl-dev libffi-dev libncurses5-dev zlib1g zlib1g-dev libreadline-dev libbz2-dev libsqlite3-dev make gcc -y &&  apt install pigz -y &&  apt install python2 -y &&  apt install python3 -y &&  apt install cpio -y &&  apt install lld -y &&  apt install llvm -y
    apt -y install libncurses5
    apt -y install rsync
    apt -y install repo
-  apt install nano bc bison ca-certificates curl flex gcc git libc6-dev libssl-dev openssl python-is-python3 ssh wget zip zstd  make clang gcc-arm-linux-gnueabi software-properties-common build-essential libarchive-tools gcc-aarch64-linux-gnu -y &&  apt install build-essential -y &&  apt install libssl-dev libffi-dev libncurses5-dev zlib1g zlib1g-dev libreadline-dev libbz2-dev libsqlite3-dev make gcc -y &&  apt install pigz -y &&  apt install python2 -y &&  apt install python3 -y &&  apt install cpio -y &&  apt install lld -y &&  apt install llvm -y
-         
-         
    
    
         git config --global user.name "Massatrio16"
@@ -46,7 +44,7 @@ read Build_Target
         
         git clone ${Device_tree} -b ${Branch_dt_twrp} ${Device_Path}
         
-        cd ${Device_Path}; export ALLOW_MISSING_DEPENDENCIES=true; . build/envsetup.sh; lunch twrp_${Device_Name}-eng; mka ${Build_Target}image
+         export ALLOW_MISSING_DEPENDENCIES=true; . build/envsetup.sh; cd ${Device_Path}; lunch twrp_${Device_Name}-eng; mka ${Build_Target}image
         
        if [ "${Build_Target}" = "vendorboot" ]; then
     Build_Target = vendor_boot
