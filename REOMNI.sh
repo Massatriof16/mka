0ff38b5a14ba9d089896278977bb2121f6baba7c
Massatrio16 <146039181+Massatrio16@users.noreply.github.com>
2024-06-28 19:33:59 +0700

    Update REOMNI.sh

--- REOMNI.sh
+++ REOMNI.sh
@@ -1,15 +1,36 @@
 current_directory = ${pwd}
  
-   echo "DEVICE TREE : "
- read DEVICE_TREE
- echo "Branch device tree twrp : "
- read Branch_dt_twrp
- echo "Device Path : "
- read Device_Path
- echo "Device Name : "
- read Device_Name
- echo "Build Target (recovery,boot) : "
- read Build_Target
+  
+echo "Link Device tree twrp : "
+read Device_tree
+if [ -z "${Device_tree}" ]; then
+    echo "Input Device tree Kosong !"
+    exit 1
+fi
+echo "Branch Device_tree_twrp : "
+read Branch_dt_twrp
+if [ -z "${Branch_dt_twrp}" ]; then
+    echo "Input branch device tree Kosong !"
+    exit 1
+fi
+echo "Device Path : "
+read Device_Path
+if [ -z "${Device_Path}" ]; then
+    echo "Input Device path Kosong!"
+    exit 1
+fi
+echo "Device Name : "
+read Device_Name
+if [ -z "{$Device_Name}" ]; then
+    echo "Input Device Name Kosong!"
+    exit 1
+fi
+echo "Build Target (recovery,boot,vendorboot) : "
+read Build_Target
+ if [ -z "${Build_Target}" ]; then
+    echo "Input Build Target Kosong!"
+    exit 1
+fi
 
 
 rm -rf /.workspace/twrp/device
