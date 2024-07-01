bb8c7aaa9b462e637c094c97703ae5afb5213299
Massatrio16 <146039181+Massatrio16@users.noreply.github.com>
2024-07-01 16:33:00 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -266,9 +266,9 @@ echo " "
         # Menyalin Hasil Build Ke direktori saat ini 
         
        if [ "${Build_Target}" = "vendorboot" ]; then
-         if [ -e " ../../../out/target/product/${Device_Name}/vendor_boot.img " ]; then
+         if [ -e " /.workspace/twrp/out/target/product/${Device_Name}/vendor_boot.img " ]; then
        
-         cp -r ../../../out/target/product/${Device_Name}/vendor_boot.img ${current_directory}
+         cp -r /.workspace/twrp/out/target/product/${Device_Name}/vendor_boot.img ${current_directory}
          
          else
          echo " "
@@ -282,8 +282,8 @@ echo " "
          
          else
          
-         if [ -e "../../../out/target/product/${Device_Name}/${Build_Target}.img" ]; then
-         cp -r ../../../out/target/product/${Device_Name}/${Build_Target}.img ${current_directory}   
+         if [ -e "/.workspace/twrp/out/target/product/${Device_Name}/${Build_Target}.img" ]; then
+         cp -r /.workspace/twrp/out/target/product/${Device_Name}/${Build_Target}.img ${current_directory}   
          
            else
            echo " "
