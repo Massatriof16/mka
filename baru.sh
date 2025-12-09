275e737baaf799182e5278cb61d743da0447768a
Nico170420 <b170420nc@gmail.com>
2025-12-10 06:02:13 +0700

    fix pkg install

--- baru.sh
+++ baru.sh
@@ -757,7 +757,7 @@ current_directory=$(pwd)
 sed -i "s|current_directory=.*|current_directory=$current_directory|" save_settings.txt
 echo " --- Memeriksa Package ---"
 echo " "
-if ! dpkg -l python3 gperf gcc-multilib gcc-10-multilib g++-multilib g++-10-multilib libc6-dev x11proto-core-dev libx11-dev tree lib32z-dev libgl1-mesa-dev libxml2-utils xsltproc bc ccache lib32readline-dev lib32z1-dev liblz4-tool libncurses5-dev libsdl1.2-dev libwxgtk3.0-gtk3-dev libxml2 lzop pngcrush schedtool squashfs-tools imagemagick libbz2-dev lzma repo rsync ncftp qemu-user-static libstdc++-10-dev libtinfo5 &>/dev/null; then
+if ! dpkg -l python3 gperf gcc-multilib gcc-10-multilib g++-multilib g++-10-multilib libc6-dev x11proto-core-dev libx11-dev tree lib32z-dev libgl1-mesa-dev libxml2-utils xsltproc bc ccache lib32readline-dev lib32z1-dev liblz4-tool libncurses5-dev libsdl1.2-dev  libxml2 lzop pngcrush schedtool squashfs-tools imagemagick libbz2-dev lzma repo rsync ncftp qemu-user-static libstdc++-10-dev  &>/dev/null; then
     # Jika paket belum terinstal, jalankan perintah instalasi
     echo " Beberapa Package belum terinstall! "
     echo " "
